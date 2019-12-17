741c26f3d50f44de2fa37ae7b49fdced7e074b9f
Charles Bao <baocharles2001@gmail.com>
2019-12-16 16:27:25 -0800

    Initial testing

--- client.cpp
+++ client.cpp
@@ -20,11 +20,24 @@ int main(int argc, char** argv){
     int sockfd;
     struct sockaddr_in client, server;
     char buffer[1024];
-    socklen_t addr_Size;
+    socklen_t addr_size;
 
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
 	    std::cerr << "cannot create socket\n";
 	    exit(0);
     }
+
+    memset(&client, '\0', sizeof(client));
+    client.sin_family = AF_INET;
+    client.sin_port = htons(port);
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if ( bind(sockfd, (const struct sockaddr *) &client, sizeof(client)) < 0 ){ 
+        std::cerr << "cannot create socket\n";
+	    exit(0);
+    }
+    addr_size = sizeof(server);
+    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*) &server, &addr_size);
+    std::cout << "Data recieved: " << buffer << std::endl;
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,9 +1,12 @@
+//client.cpp
+//Charles Bao
 #include <iostream>
-#include <stdio.h> 
-#include <stdlib.h> 
-#include <unistd.h> 
-#include <string.h> 
-#include <sys/types.h> 
-#include <sys/socket.h> 
-#include <arpa/inet.h> 
-#include <netinet/in.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <thread>
