edeb77fc695496b70ada9efc66da63557c99212a
Charles Bao <baocharles2001@gmail.com>
2019-12-25 13:33:41 -0800

    organized code and started testing code for multi user use

--- client.cpp
+++ client.cpp
@@ -16,103 +16,121 @@
 
 struct sockaddr_in server;
 int sockfd;
-char* getData(std::ifstream &in);
-void reader();
-std::string readMessage();
-void sender();
-void sendMessage(std::string message, struct sockaddr_in recipient);
-void initClient(int argc, char** argv);
 
-char* getData(std::ifstream &in){
-    std::string data;
-    getline(in,data);
-    data = data.substr(15);
-    int n = data.length() + 1; //convert from a string to char array
-    char* charArr = new char[n];
-    strcpy(charArr, data.c_str());
-    return charArr;
-}//takes in data from the file
-
-void receiver(){
-    bool running = true;
-    std::string message;
-    while(running){
-        message = readMessage();
-        std::cout << "data recieved: " << message << std::endl;
-        if(message == "end"){
-            running = false;
-            break;
+class messageManip{
+    public:
+    static std::string addHeader(const std::string& header, const std::string& message){
+        std::string headerAndMessage = header;
+        while(headerAndMessage.size() < 15){
+            headerAndMessage += "-";
         }
+        headerAndMessage += message;
+        return headerAndMessage;
     }
-}//constantly checks for new data being sent to client
 
-std::string readMessage(){
-    char buffer[1024];
-    socklen_t addr_size = sizeof(server);
-    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*) &server, &addr_size);
-    std::string message(buffer);
-    return message;
-}//reads a message
-
-void sender(){
-    bool running = true; 
-    std::string message;
-    while(running){
-        std::getline(std::cin,message);
-        sendMessage(message, server);
-        if(message == "end"){
-            running = false;
-            break;
+    static std::string addHeader(const std::string& header, int data, const std::string& message){
+        std::string headerAndMessage = header + std::to_string(data);
+        while(headerAndMessage.size() < 15){
+            headerAndMessage += "-";
         }
-        std::cout << "Data sent: "<< message <<std::endl;
+        headerAndMessage += message;
+        return headerAndMessage;
     }
-}//sends out new data
+};
 
-void sendMessage(std::string message, struct sockaddr_in recipient){
-    char buffer[1024];
-    strcpy(buffer, message.c_str());
-    sendto(sockfd, buffer, 1024, 0, (struct sockaddr*) &recipient, sizeof(recipient));
-}
+class networkIO{
+    public:
+    static void reader(){
+        bool running = true;
+        std::string message;
+        while(running){
+            message = readMessage();
+            std::cout << "data recieved: " << message << std::endl;
+            if(message == "end"){
+                running = false;
+                break;
+            }
+        }
+    }//constantly checks for new data being sent to client
 
-void initClient(int argc, char** argv){
-    if(argc != 2){
-        std::cerr << "Usage:" << argv[0] << " ServerInfo.txt\n";
-        exit(0);
-    }//check to make sure the server info file was passed to main
+    static std::string readMessage(){
+        char buffer[1024];
+        socklen_t addr_size = sizeof(server);
+        recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*) &server, &addr_size);
+        std::string message(buffer);
+        return message;
+    }//reads and returns a message
 
-    std::ifstream in;
-    in.open(argv[1]);
-    if(!in){
-        std::cerr << "Error opening the file\n";
-        exit(0);
-    }//check to see if the file can be opened
-    char* portDataFF = getData(in);
-    char* addressDataFF = getData(in);
+    static void sender(){
+        bool running = true; 
+        std::string message;
+        while(running){
+            std::getline(std::cin,message);
+            sendMessage(message, server);
+            if(message == "end"){
+                running = false;
+                break;
+            }
+            std::cout << "Data sent: "<< message <<std::endl;
+        }
+    }//sends out data inputted by the user
 
-    int port = atoi(portDataFF);
+    static void sendMessage(std::string message, struct sockaddr_in recipient){
+        char buffer[1024];
+        strcpy(buffer, message.c_str());
+        sendto(sockfd, buffer, 1024, 0, (struct sockaddr*) &recipient, sizeof(recipient));
+    }//sends a message
+};
 
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-	    std::cerr << "Cannot create socket\n";
-	    exit(0);
-    }//check to see if socket was properly created
+class misc{
+    public:
+    static void initClient(int argc, char** argv){
+        if(argc != 2){
+            std::cerr << "Usage:" << argv[0] << " ServerInfo.txt\n";
+            exit(0);
+        }//check to make sure the server info file was passed to main
 
-    memset(&server, '\0', sizeof(server));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = inet_addr(addressDataFF);
-    delete [] portDataFF;
-    delete [] addressDataFF;
-}//initializes client socket and data
+        std::ifstream in;
+        in.open(argv[1]);
+        if(!in){
+            std::cerr << "Error opening the file\n";
+            exit(0);
+        }//check to see if the file can be opened
+        char* portDataFF = readFromFile(in);
+        char* addressDataFF = readFromFile(in);
+
+        int port = atoi(portDataFF);
+
+        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            std::cerr << "Cannot create socket\n";
+            exit(0);
+        }//check to see if socket was properly created
+
+        memset(&server, '\0', sizeof(server));
+        server.sin_family = AF_INET;
+        server.sin_port = htons(port);
+        server.sin_addr.s_addr = inet_addr(addressDataFF);
+        delete [] portDataFF;
+        delete [] addressDataFF;
+        networkIO::sendMessage("/login", server);
+    }//initializes client socket and data
+
+    static char* readFromFile(std::ifstream &in){
+        std::string data;
+        getline(in,data);
+        data = data.substr(15);
+        int n = data.length() + 1; //convert from a string to char array
+        char* charArr = new char[n];
+        strcpy(charArr, data.c_str());
+        return charArr;
+    }//takes in data from the file
+};
 
 int main(int argc, char** argv){
-    initClient(argc,argv);
-
-    sendMessage("/login", server);
-    
-    std::thread rfs(receiver);
+    misc::initClient(argc,argv);
+    std::thread rfs(networkIO::reader);
     rfs.detach();
-    std::thread sts(sender);
+    std::thread sts(networkIO::sender);
     sts.join();
-    
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,6 +1,6 @@
 //server.cpp
 //Charles Bao
-//currently works as a client->server->client chat for 2 clients
+
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -18,15 +18,6 @@
 struct sockaddr_in sendSock, server;
 int sockfd;
 struct users* userInfo;
-void initServer(int argc, char** argv);
-char* getData(std::ifstream &in);
-void sendMessage(std::string message, struct sockaddr_in recipient);
-std::string readMessage();
-void messageRouter(struct users& userInfo);
-struct users* initUsers();
-void deleteUsers();
-bool compareSock(struct sockaddr_in user1, struct sockaddr_in user2);
-void sendOtherUser(std::string message, struct users* userInfo);
 
 struct user{
     struct sockaddr_in clientSocket;
@@ -37,125 +28,155 @@ struct users{
     struct user* user2;
 };//temp for testing
 
-void initServer(int argc, char** argv){
-    if(argc != 2){
-        std::cerr << "Usage:" << argv[0] << " ServerInfo.txt\n";
-        exit(0);
-    }//check to make sure the server info file was passed to main
+class messageManip{
+    public:
+    static std::string addHeader(const std::string& header, const std::string& message){
+        std::string headerAndMessage = header;
+        while(headerAndMessage.size() < 15){
+            headerAndMessage += "-";
+        }
+        headerAndMessage += message;
+        return headerAndMessage;
+    }
 
-    std::ifstream in;
-    in.open(argv[1]);
-    if(!in){
-        std::cerr << "Error opening the file\n";
-        exit(0);
-    }//check to see if the file can be opened
-    char* portData = getData(in);
-    char* addressData = getData(in);
-    
-    int port = atoi(portData);
-    delete [] portData;
+    static std::string addHeader(const std::string& header, int data, const std::string& message){
+        std::string headerAndMessage = header + std::to_string(data);
+        while(headerAndMessage.size() < 15){
+            headerAndMessage += "-";
+        }
+        headerAndMessage += message;
+        return headerAndMessage;
+    }
+};
 
-    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-	    std::cerr << "Cannot create socket\n";
-	    exit(0);
-    }//check to see if socket was properly created
+class networkIO{
+    public:
+    static void sendMessage(std::string message, struct sockaddr_in recipient){
+        char buffer[1024];
+        strcpy(buffer, message.c_str());
+        sendto(sockfd, buffer, 1024, 0, (struct sockaddr*) &recipient, sizeof(recipient));
+    }//sends a message
 
-    memset(&sendSock, '\0', sizeof(sendSock));
-    server.sin_family = AF_INET;
-    server.sin_port = htons(port);
-    server.sin_addr.s_addr = inet_addr(addressData);
-    delete [] addressData;
+    static std::string readMessage(){
+        char buffer[1024];
+        socklen_t addr_size = sizeof(sendSock);
+        recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*) &sendSock, &addr_size);
+        std::string message(buffer);
+        return message;
+    }//reads and returns a message
 
-    if ( bind(sockfd, (const struct sockaddr *) &server, sizeof(server)) < 0 ){ 
-        std::cerr << "Cannot bind socket\n";
-	    exit(0);
-    }//check to see if the socket was properly binded
-}
-
-char* getData(std::ifstream &in){
-    std::string data;
-    getline(in,data);
-    data = data.substr(15);
-    int n = data.length() + 1; //convert from a string to char array
-    char* charArr = new char[n];
-    strcpy(charArr, data.c_str());
-    return charArr;
-}//takes in data from the file
-
-void sendMessage(std::string message, struct sockaddr_in recipient){
-    char buffer[1024];
-    strcpy(buffer, message.c_str());
-    sendto(sockfd, buffer, 1024, 0, (struct sockaddr*) &recipient, sizeof(recipient));
-}//sends a message
-
-std::string readMessage(){
-    char buffer[1024];
-    socklen_t addr_size = sizeof(sendSock);
-    recvfrom(sockfd, buffer, 1024, 0, (struct sockaddr*) &sendSock, &addr_size);
-    std::string message(buffer);
-    return message;
-}//reads a message
-
-void messageRouter(struct users* userInfo){
-    bool running = true;
-    while(running){
-        std::string message = readMessage();
-        char IP[INET_ADDRSTRLEN];
-        inet_ntop(AF_INET, &(sendSock.sin_addr), IP, INET_ADDRSTRLEN);
-        std::cout << "data recieved: " << IP << "-" << sendSock.sin_port << ": " << message << std::endl;
-        if(message == "end"){
-                running = false;
-                break;
+    static void messageRouter(){
+        bool running = true;
+        while(running){
+            std::string message = readMessage();
+            char IP[INET_ADDRSTRLEN];
+            inet_ntop(AF_INET, &(sendSock.sin_addr), IP, INET_ADDRSTRLEN);
+            std::cout << "data recieved: " << IP << "-" << sendSock.sin_port << ": " << message << std::endl;
+            if(message == "end"){
+                    running = false;
+                    break;
+                }
+            if(message == "/login"){
+                if(userInfo->user1 == NULL){
+                    userInfo->user1 = new user{sendSock};
+                    sendMessage("Connected to server", sendSock);
+                }
+                else if(userInfo->user2 == NULL){
+                    userInfo->user2 = new user{sendSock};
+                    sendMessage("Connected to server", sendSock);
+                }
+                else{
+                    sendMessage("There are already 2 people chatting",sendSock);
+                }
             }
-        if(message == "/login"){
-            if(userInfo->user1 == NULL){
-                userInfo->user1 = new user{sendSock};
-                sendMessage("Connected to server", sendSock);
-            }
-            else if(userInfo->user2 == NULL){
-                userInfo->user2 = new user{sendSock};
-                sendMessage("Connected to server", sendSock);
-            }
-            else{
-                sendMessage("There are already 2 people chatting",sendSock);
+            else if(userInfo->user1 != NULL && userInfo->user2 != NULL){
+                sendOtherUser(message);
             }
         }
-        else if(userInfo->user1 != NULL && userInfo->user2 != NULL){
-            sendOtherUser(message, userInfo);
+    }//redirects all the messages to the other person 
+
+    static void sendOtherUser(std::string message){
+        struct sockaddr_in user1 = userInfo->user1->clientSocket;
+        struct sockaddr_in user2 = userInfo->user2->clientSocket;
+        if(compareSock(sendSock,user1)){
+            sendMessage(message,user2);
         }
+        else if(compareSock(sendSock,user2)){
+            sendMessage(message,user1);
+        }
+    }//sends the message to the other user (temp for testing)
+
+    static bool compareSock(struct sockaddr_in user1, struct sockaddr_in user2){
+        return (user1.sin_addr.s_addr == user2.sin_addr.s_addr && user1.sin_port == user2.sin_port);
+    }// compares sockets
+};
+
+class misc{
+    public:
+    static void initServer(int argc, char** argv){
+        if(argc != 2){
+            std::cerr << "Usage:" << argv[0] << " ServerInfo.txt\n";
+            exit(0);
+        }//check to make sure the server info file was passed to main
+
+        std::ifstream in;
+        in.open(argv[1]);
+        if(!in){
+            std::cerr << "Error opening the file\n";
+            exit(0);
+        }//check to see if the file can be opened
+
+        char* portData = readFromFile(in);
+        char* addressData = readFromFile(in);
+        int port = atoi(portData);
+
+        if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+            std::cerr << "Cannot create socket\n";
+            exit(0);
+        }//check to see if socket was properly created
+
+        memset(&sendSock, '\0', sizeof(sendSock));
+        server.sin_family = AF_INET;
+        server.sin_port = htons(port);
+        server.sin_addr.s_addr = inet_addr(addressData);
+
+        if (bind(sockfd, (const struct sockaddr *) &server, sizeof(server)) < 0){ 
+            std::cerr << "Cannot bind socket\n";
+            exit(0);
+        }//check to see if the socket was properly binded
+        delete [] portData;
+        delete [] addressData;
     }
-}//redirects all the messages to the other person 
 
-struct users* initUsers(){
-    struct users* userInfo= new users{NULL,NULL};
-    return userInfo;
-}//creates struct containing user info
+    static char* readFromFile(std::ifstream &in){
+        std::string data;
+        getline(in,data);
+        data = data.substr(15);
+        int n = data.length() + 1; //convert from a string to char array
+        char* charArr = new char[n];
+        strcpy(charArr, data.c_str());
+        return charArr;
+    }//takes in data from the file
+};
 
-void deleteUsers(struct users* userInfo){
-    delete userInfo->user1;
-    delete userInfo->user2;
-    delete userInfo;
-}//deletes all user info
+class userData{
+    public:
+    static struct users* initUsers(){
+        struct users* userInfo= new users{NULL,NULL};
+        return userInfo;
+    }//creates struct containing user info
 
-bool compareSock(struct sockaddr_in user1, struct sockaddr_in user2){
-    return (user1.sin_addr.s_addr == user2.sin_addr.s_addr && user1.sin_port == user2.sin_port);
-}
-
-void sendOtherUser(std::string message, struct users* userInfo){
-    struct sockaddr_in user1 = userInfo->user1->clientSocket;
-    struct sockaddr_in user2 = userInfo->user2->clientSocket;
-    if(compareSock(sendSock,user1)){
-        sendMessage(message,user2);
-    }
-    else if(compareSock(sendSock,user2)){
-        sendMessage(message,user1);
-    }
-}//sends the message to the other user (temp for testing)
+    static void deleteAllUsers(){
+        delete userInfo->user1;
+        delete userInfo->user2;
+        delete userInfo;
+    }//deletes all user info
+};
 
 int main(int argc, char** argv){
-    initServer(argc,argv);
-    userInfo = initUsers();
-    messageRouter(userInfo);
-    deleteUsers(userInfo);
+    misc::initServer(argc,argv);
+    userInfo = userData::initUsers();
+    networkIO::messageRouter();
+    userData::deleteAllUsers();
     return 0;
 }
